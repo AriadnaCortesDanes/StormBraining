f2d8c72b8f28739529d82fa72e88e7b30b15d378
marcordonez <marc.ordonez@estudiantat.upc.edu>
2022-05-01 08:08:06 +0200

    responsive i alineat

--- popup.js
+++ popup.js
@@ -2,20 +2,20 @@ var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
 	overlay = document.getElementById('overlay'),
 	popup = document.getElementById('popup'),
 	btnCerrarPopup = document.getElementById('btn-cerrar-popup'),
-    btnStart = document.getElementById('btn-start');
+	btnStart = document.getElementById('btn-start');
 
-btnAbrirPopup.addEventListener('click', function(){
+btnAbrirPopup.addEventListener('click', function () {
 	overlay.classList.add('active');
 	popup.classList.add('active');
 });
 
-btnCerrarPopup.addEventListener('click', function(e){
+btnCerrarPopup.addEventListener('click', function (e) {
 	e.preventDefault();
 	overlay.classList.remove('active');
 	popup.classList.remove('active');
 })
 
-btnStart.addEventListener('click', function(e){
+btnStart.addEventListener('click', function (e) {
 	e.preventDefault();
 	overlay.classList.remove('active');
 	popup.classList.remove('active');
